8b786babecda021094ebfe84bbe926c09d4ac1c6
HaowenWeiJohn <59973823+HaowenWeiJohn@users.noreply.github.com>
2023-11-08 17:07:34 -0500

    update all

--- Assets/Scripts/KeyBoard/KeyController.cs
+++ Assets/Scripts/KeyBoard/KeyController.cs
@@ -6,145 +6,172 @@ using UnityEngine.UI;
 
 public class KeyController : MonoBehaviour
 {
-    // TPM text field
-    public TextMeshProUGUI textMesh;
-    public RawImage bgImage;
 
-    public bool hasGazeThisFrame = false;
-    public float dwellCounter;
+    
 
-    // add an audio source
-    GameSettings gameSettings;
-    InputFieldController inputFieldController;
-    bool selected = false;
+    //// TPM text field
+    //public TextMeshProUGUI textMesh;
+    //public RawImage bgImage;
 
-    ParamsArchive.InteractionMode prevInteractionMode;
+    //public bool hasGazeThisFrame = false;
+    //public float dwellCounter;
 
-    // Start is called before the first frame update
+    //// add an audio source
+    //GameSettings gameSettings;
+    //InputFieldController inputFieldController;
+    //bool selected = false;
+
+    //ParamsArchive.InteractionMode prevInteractionMode;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    gameSettings = GameObject.Find("GameSettings").GetComponent<GameSettings>();
+    //    inputFieldController = GameObject.Find("UIController").GetComponent<InputFieldController>();
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (prevInteractionMode != gameSettings.interactionMode)
+    //    {
+    //        // reset the dwell counter
+    //        dwellCounter = 0;
+    //        // set color to white
+    //        bgImage.color = Color.white;
+    //        // reset the selected flag
+    //        selected = false;
+    //    }
+
+    //    // check the interaction mode
+    //    if (gameSettings.interactionMode == ParamsArchive.InteractionMode.button)
+    //    {
+    //        ButtonInteraction();
+    //    }
+
+    //    else if (gameSettings.interactionMode == ParamsArchive.InteractionMode.dwellTime)
+    //    {
+    //        DwellTimeInteraction();
+    //    }
+    //    else if (gameSettings.interactionMode == ParamsArchive.InteractionMode.VEP)
+    //    {
+    //        VEPInteraction();
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Invalid interaction mode!");
+    //    }
+
+    //    prevInteractionMode = gameSettings.interactionMode;
+    //    hasGazeThisFrame = false;  // reset the flag
+    //}
+
+    //public void SetChar(string character)
+    //{
+    //    textMesh.text = character;
+    //}
+
+    //public void HasGaze()
+    //{
+    //    hasGazeThisFrame = true;
+    //}
+
+    //void ButtonInteraction()
+    //{
+    //    if (hasGazeThisFrame)
+    //    {
+    //        if (!selected)
+    //        {
+    //            // play the audio clip
+    //            AudioSource.PlayClipAtPoint(gameSettings.keyHoverAudioClip, transform.position);
+    //        }
+    //        // set bg image to red
+    //        bgImage.color = Color.red;
+    //        selected = true;
+
+    //        // if the space key is pressed
+    //        if (Input.GetKeyDown(KeyCode.Space))
+    //        {
+    //            // play the audio clip
+    //            AudioSource.PlayClipAtPoint(gameSettings.keyPressAudioClip, transform.position);
+    //            inputFieldController.GetComponent<InputFieldController>().AddText(textMesh.text);
+    //        }
+    //    }
+    //    else
+    //    {
+    //        bgImage.color = Color.white;
+    //        selected = false;
+    //    }
+    //}
+
+    //void DwellTimeInteraction()
+    //{
+    //    if (hasGazeThisFrame)
+    //    {
+    //        if (!selected)
+    //        {
+    //            // play the audio clip
+    //            AudioSource.PlayClipAtPoint(gameSettings.keyHoverAudioClip, transform.position);
+    //        }
+
+    //        // increment the dwell counter
+    //        dwellCounter += Time.deltaTime;
+
+    //        // lerp the color from white to red
+    //        bgImage.color = Color.Lerp(Color.white, Color.red, dwellCounter / gameSettings.dwellActivateTime);
+
+    //        // if the dwell counter is greater than the dwell time
+    //        if (dwellCounter > gameSettings.dwellActivateTime)
+    //        {
+    //            // play the audio clip
+    //            AudioSource.PlayClipAtPoint(gameSettings.keyPressAudioClip, transform.position);
+    //            inputFieldController.GetComponent<InputFieldController>().AddText(textMesh.text);
+    //            dwellCounter = 0;
+    //        }
+    //        // selected = true;
+    //    }
+    //    else
+    //    {
+    //        dwellCounter = 0;
+    //        bgImage.color = Color.white;
+    //        selected = false;
+    //    }
+    //}
+
+    //void VEPInteraction()
+    //{
+    //    // flash the bg image between white and light grey at random intervals
+    //    if (Random.Range(0, 100) < 50)
+    //    {
+    //        bgImage.color = Color.Lerp(Color.white, Color.grey, Random.Range(0.0f, 0.2f));
+    //    }
+    //    else
+    //    {
+    //        bgImage.color = Color.white;
+    //    }
+    //}
+
+
+    public List<KeyController> keyControllers = new List<KeyController>();
+
+
+
     void Start()
     {
-        gameSettings = GameObject.Find("GameSettings").GetComponent<GameSettings>();
-        inputFieldController = GameObject.Find("UIController").GetComponent<InputFieldController>();
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        if (prevInteractionMode != gameSettings.interactionMode)
-        {
-            // reset the dwell counter
-            dwellCounter = 0;
-            // set color to white
-            bgImage.color = Color.white;
-            // reset the selected flag
-            selected = false;
-        }
-
-        // check the interaction mode
-        if (gameSettings.interactionMode == ParamsArchive.InteractionMode.button)
-        {
-            ButtonInteraction();
-        }
-
-        else if (gameSettings.interactionMode == ParamsArchive.InteractionMode.dwellTime)
-        {
-            DwellTimeInteraction();
-        }
-        else if (gameSettings.interactionMode == ParamsArchive.InteractionMode.VEP)
-        {
-            VEPInteraction();
-        }
-        else
-        {
-            Debug.Log("Invalid interaction mode!");
-        }
-
-        prevInteractionMode = gameSettings.interactionMode;
-        hasGazeThisFrame = false;  // reset the flag
+        
     }
 
-    public void SetChar(string character)
+    public void initChars()
     {
-        textMesh.text = character;
+
     }
 
-    public void HasGaze()
-    {
-        hasGazeThisFrame = true;
-    }
 
-    void ButtonInteraction()
-    {
-        if (hasGazeThisFrame)
-        {
-            if (!selected)
-            {
-                // play the audio clip
-                AudioSource.PlayClipAtPoint(gameSettings.keyHoverAudioClip, transform.position);
-            }
-            // set bg image to red
-            bgImage.color = Color.red;
-            selected = true;
 
-            // if the space key is pressed
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                // play the audio clip
-                AudioSource.PlayClipAtPoint(gameSettings.keyPressAudioClip, transform.position);
-                inputFieldController.GetComponent<InputFieldController>().AddText(textMesh.text);
-            }
-        }
-        else
-        {
-            bgImage.color = Color.white;
-            selected = false;
-        }
-    }
-
-    void DwellTimeInteraction()
-    {
-        if (hasGazeThisFrame)
-        {
-            if (!selected)
-            {
-                // play the audio clip
-                AudioSource.PlayClipAtPoint(gameSettings.keyHoverAudioClip, transform.position);
-            }
-
-            // increment the dwell counter
-            dwellCounter += Time.deltaTime;
-
-            // lerp the color from white to red
-            bgImage.color = Color.Lerp(Color.white, Color.red, dwellCounter / gameSettings.dwellActivateTime);
-
-            // if the dwell counter is greater than the dwell time
-            if (dwellCounter > gameSettings.dwellActivateTime)
-            {
-                // play the audio clip
-                AudioSource.PlayClipAtPoint(gameSettings.keyPressAudioClip, transform.position);
-                inputFieldController.GetComponent<InputFieldController>().AddText(textMesh.text);
-                dwellCounter = 0;
-            }
-            // selected = true;
-        }
-        else
-        {
-            dwellCounter = 0;
-            bgImage.color = Color.white;
-            selected = false;
-        }
-    }
-
-    void VEPInteraction()
-    {
-        // flash the bg image between white and light grey at random intervals
-        if (Random.Range(0, 100) < 50)
-        {
-            bgImage.color = Color.Lerp(Color.white, Color.grey, Random.Range(0.0f, 0.2f));
-        }
-        else
-        {
-            bgImage.color = Color.white;
-        }
-    }
 }
--- Assets/Scripts/Presets/Params.cs
+++ Assets/Scripts/Presets/Params.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,117 @@ using UnityEngine;
 public static class Params
 {
 
-    public static List<List<string>> LetterOrders = new List<List<string>> {
-        new List<string> { "Z", "X", "C", "V", "B", "N", "M" },
-        new List<string> { "A", "S", "D", "F", "G", "H", "J", "K", "L" },
-        new List<string> { "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P" }
+
+
+
+
+
+
+    public enum Keys
+    {
+        A = 1,
+        B = 2,
+        C = 3,
+        D = 4,
+        E = 5,
+        F = 6,
+        G = 7,
+        H = 8,
+        I = 9,
+        J = 10,
+        K = 11,
+        L = 12,
+        M = 13,
+        N = 14,
+        O = 15,
+        P = 16,
+        Q = 17,
+        R = 18,
+        S = 19,
+        T = 20,
+        U = 21,
+        V = 22,
+        W = 23,
+        X = 24,
+        Y = 25,
+        Z = 26
+    }
+
+    public static List<List<Keys>> CharsOrders = new List<List<Keys>> {
+    new List<Keys> { Keys.Q, Keys.W, Keys.E, Keys.R, Keys.T, Keys.Y, Keys.U, Keys.I, Keys.O, Keys.P },
+    new List<Keys> { Keys.A, Keys.S, Keys.D, Keys.F, Keys.G, Keys.H, Keys.J, Keys.K, Keys.L },
+    new List<Keys> { Keys.Z, Keys.X, Keys.C, Keys.V, Keys.B, Keys.N, Keys.M }
     };
+
+    public static Vector2[] CharOffset = new Vector2[]
+    {
+            new Vector2(0.0f, 0.0f),
+            new Vector2(0.25f, -1.0f),
+            new Vector2(0.8f, -2.0f)
+    };
+
+
+    public static Dictionary<Keys, int> KeysID = new Dictionary<Keys, int>
+    {
+        [Keys.A] = 1,
+        [Keys.B] = 2,
+        [Keys.C] = 3,
+        [Keys.D] = 4,
+        [Keys.E] = 5,
+        [Keys.F] = 6,
+        [Keys.G] = 7,
+        [Keys.H] = 8,
+        [Keys.I] = 9,
+        [Keys.J] = 10,
+        [Keys.K] = 11,
+        [Keys.L] = 12,
+        [Keys.M] = 13,
+        [Keys.N] = 14,
+        [Keys.O] = 15,
+        [Keys.P] = 16,
+        [Keys.Q] = 17,
+        [Keys.R] = 18,
+        [Keys.S] = 19,
+        [Keys.T] = 20,
+        [Keys.U] = 21,
+        [Keys.V] = 22,
+        [Keys.W] = 23,
+        [Keys.X] = 24,
+        [Keys.Y] = 25,
+        [Keys.Z] = 26
+    };
+
+
+    public static Dictionary<Keys, string> KeysString = new Dictionary<Keys, string>
+    {
+        [Keys.A] = "A",
+        [Keys.B] = "B",
+        [Keys.C] = "C",
+        [Keys.D] = "D",
+        [Keys.E] = "E",
+        [Keys.F] = "F",
+        [Keys.G] = "G",
+        [Keys.H] = "H",
+        [Keys.I] = "I",
+        [Keys.J] = "J",
+        [Keys.K] = "K",
+        [Keys.L] = "L",
+        [Keys.M] = "M",
+        [Keys.N] = "N",
+        [Keys.O] = "O",
+        [Keys.P] = "P",
+        [Keys.Q] = "Q",
+        [Keys.R] = "R",
+        [Keys.S] = "S",
+        [Keys.T] = "T",
+        [Keys.U] = "U",
+        [Keys.V] = "V",
+        [Keys.W] = "W",
+        [Keys.X] = "X",
+        [Keys.Y] = "Y",
+        [Keys.Z] = "Z"
+    };
+
 
 
     // define an enum for different interaction modes
@@ -20,7 +127,6 @@ public static class Params
         IllumiReadSwype = 3,
         FreeSwitch = 4
     }
-
 
 
 }
